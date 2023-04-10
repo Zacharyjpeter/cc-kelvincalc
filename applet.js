0b55d96fb4bfef088bb7f6a8ff36f99c9cbc17ab
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-04-10 17:14:21 -0500

    fixed math floor and added other features

--- applet.js
+++ applet.js
@@ -1,10 +1,16 @@
 //gives variable kelvin value of 293
-const kelvin = 300;
+const kelvin = 290;
 //calculates celsius based on kelvin
 let celsius = kelvin - 273;
 //calculates fahrenheit based on celsius
 let fahrenheit = celsius * (9/5) + 32;
 //makes fahrenheit a whole number
-let temperature = math.floor(fahrenheit);
+let temperature = (Math.floor(fahrenheit));
+//prints the temp in kelvin
+console.log(`The original temperature is ${kelvin} kelvin`);
 //prints the temp in fahrenheit
-console.log(`The temperature is ${temperature} degrees Fahrenheit`);
+console.log(`That is ${temperature} degrees Fahrenheit`);
+//prints the temp in newton
+let newton = celsius * (33/100);
+newton = Math.floor(newton)
+console.log(`And ${newton} Newton`);
